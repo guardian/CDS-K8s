afdfde620af964fe43ad7ea9229bde8eeff5a115
Dave Allison <david.allison@theguardian.com>
2022-03-10 17:18:01 +0000

    Fix loading of useHistory

--- cdslogviewer/frontend/app/LogByJobName.tsx
+++ cdslogviewer/frontend/app/LogByJobName.tsx
@@ -1,5 +1,5 @@
-import React, { useHistory } from "react";
-import { RouteComponentProps, useParams } from "react-router";
+import React from "react";
+import { RouteComponentProps, useParams, useHistory } from "react-router";
 import { loadLogForJobNameURL } from "./data-loading";
 
 const LogByJobName: React.FC<RouteComponentProps> = (props) => {
